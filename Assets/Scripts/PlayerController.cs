607eabceb373ea0c5c4bc996e50fd3d559a6204c
WuBoytH <alexwuief@gmail.com>
2021-01-28 14:26:04 -0500

    Added OnMove function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +10,8 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    voice OnMove(InputValue movementValue)
     {
-        
+        // Function Body
     }
 }
