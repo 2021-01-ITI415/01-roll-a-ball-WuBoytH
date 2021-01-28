dc9dd789692ba8d0a265c8c044caf10779ee4001
WuBoytH <alexwuief@gmail.com>
2021-01-28 14:38:12 -0500

    Set up player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-    voice OnMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         // Function Body
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
     }
 }
